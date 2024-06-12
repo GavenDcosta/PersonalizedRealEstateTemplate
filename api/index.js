024259968fd846b3595cb2752e87e1cec405722a
Gaven Dcosta <gavendcosta@gmail.com>
2024-06-12 08:21:37 +0530

    cors changes

--- api/index.js
+++ api/index.js
@@ -9,10 +9,7 @@ import prisma from "./lib/prisma.js"
 
 const app = express()
 
-app.use(cors({
-    origin: '*',
-}))
-
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
 app.use(express.json())
 app.use(cookieParser())
 
