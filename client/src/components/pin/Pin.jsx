91bc6f8f82ed30d0952bcfed4f8753acf152f50f
Gaven Dcosta <gavendcosta@gmail.com>
2024-06-12 12:10:01 +0530

    map

--- client/src/components/pin/Pin.jsx
+++ client/src/components/pin/Pin.jsx
@@ -1,19 +1,10 @@
 import { Marker, Popup } from "react-leaflet";
-import L from "leaflet"; // Make sure to import Leaflet library
-import {Link} from "react-router-dom"
-import markerImg from "./marker-icon-2x.png";
+import "./pin.scss";
+import { Link } from "react-router-dom";
 
 function Pin({ item }) {
-  // Create an icon instance
-  const customIcon = L.icon({
-    iconUrl: markerImg,
-    iconSize: [25, 41], // Size of the icon
-    iconAnchor: [12, 41], // Point of the icon which will correspond to marker's location
-    popupAnchor: [1, -34], // Point from which the popup should open relative to the iconAnchor
-  });
-
   return (
-    <Marker position={[item.latitude, item.longitude]} icon={customIcon}>
+    <Marker position={[item.latitude, item.longitude]}>
       <Popup>
         <div className="popupContainer">
           <img src={item.images[0]} alt="" />
