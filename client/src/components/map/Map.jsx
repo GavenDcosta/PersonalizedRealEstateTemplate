e763493024dd9e425b87e86390a7a71d7df203e6
Gaven Dcosta <gavendcosta@gmail.com>
2024-06-12 08:18:48 +0530

    leaflet css

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./map.scss"
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import "leaflet/dist/leaflet.css/"
+import "./leaflet.css"
 import Pin from '../pin/Pin'
   
 const Map = ({items}) => {
