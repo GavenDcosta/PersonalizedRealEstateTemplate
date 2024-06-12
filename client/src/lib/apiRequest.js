f5d51d45f823db09ed4eea41e258386075ab2eb0
Gaven Dcosta <gavendcosta@gmail.com>
2024-06-12 08:23:42 +0530

    added hosted api to proxy and ui

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiRequest = axios.create({
-    baseURL: "http://localhost:8800/api",
+    baseURL: "https://mandg-personalized-real-estate-api.vercel.app/api",
     withCredentials: true,
 })
 
